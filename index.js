462547c98ef8cacceaac82190444772a3b7053bd
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-08 11:33:09 +0530

    Add turn functionality

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('checking connection')
+let origBoard = '';
+const huPlayer = 'O';
+const aiPlayer = 'X';
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
+const cells = document.querySelectorAll('.cell');
+
+const turnClick = (e) => {
+  turn(e.target.id, huPlayer);
+}
+
+const turn = (squareId, player) => {
+  origBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+}
+
+const startGame = () => {
+  document.querySelector('.endgame').style.display = 'none';
+  origBoard = Array.from(Array(9).keys());
+  cells.forEach(cell => {
+    cell.style.removeProperty('background-color');
+    cell.innerHTML = '';
+    cell.addEventListener('click', turnClick, false);
+  })
+}
+
+startGame();
